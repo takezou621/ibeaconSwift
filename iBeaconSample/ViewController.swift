97edea460946ee72194ddab0cc6a583db1cb72c7
Takeshi Kawai <takezou621@gmail.com>
2014-09-01 10:50:30 +0900

    Initial Commit

---
+++ iBeaconSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconSample
+//
+//  Created by Kawai Takeshi on 2014/09/01.
+//  Copyright (c) 2014年 Kawai Takeshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
